00c9bfaf6fb266f013356602a9ceadd16cbcfe6c
rotetsuo <rotetsuo32@gmail.com>
2022-11-18 16:27:11 -0300

    Todas as functinos funcinoando

--- robertoFormula1/Form1.Designer.cs
+++ robertoFormula1/Form1.Designer.cs
@@ -28,13 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TxbTomadaTempo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbMedia = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbMelhorVolta = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbPiorVolta = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(575, 336);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TxbTomadaTempo
+            // 
+            this.TxbTomadaTempo.Location = new System.Drawing.Point(607, 72);
+            this.TxbTomadaTempo.Name = "TxbTomadaTempo";
+            this.TxbTomadaTempo.Size = new System.Drawing.Size(158, 20);
+            this.TxbTomadaTempo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(603, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TOMADA DE TEMPO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(584, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "FORMULA 1";
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnGravar.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGravar.Location = new System.Drawing.Point(610, 112);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(158, 70);
+            this.btnGravar.TabIndex = 4;
+            this.btnGravar.Text = "GRAVAR";
+            this.btnGravar.UseVisualStyleBackColor = false;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnLimpar.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(610, 188);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(158, 72);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnFechar.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(610, 266);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(158, 71);
+            this.btnFechar.TabIndex = 6;
+            this.btnFechar.Text = "FECHAR";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 365);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "MÉDIA DE TEMPOS";
+            // 
+            // txbMedia
+            // 
+            this.txbMedia.Location = new System.Drawing.Point(12, 387);
+            this.txbMedia.Name = "txbMedia";
+            this.txbMedia.Size = new System.Drawing.Size(158, 20);
+            this.txbMedia.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(205, 365);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 19);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "MELHOR VOLTA";
+            // 
+            // txbMelhorVolta
+            // 
+            this.txbMelhorVolta.Location = new System.Drawing.Point(198, 387);
+            this.txbMelhorVolta.Name = "txbMelhorVolta";
+            this.txbMelhorVolta.Size = new System.Drawing.Size(158, 20);
+            this.txbMelhorVolta.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(418, 365);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 19);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "PIOR VOLTA";
+            // 
+            // txbPiorVolta
+            // 
+            this.txbPiorVolta.Location = new System.Drawing.Point(396, 387);
+            this.txbPiorVolta.Name = "txbPiorVolta";
+            this.txbPiorVolta.Size = new System.Drawing.Size(158, 20);
+            this.txbPiorVolta.TabIndex = 11;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnCalcular.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(607, 361);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(158, 71);
+            this.btnCalcular.TabIndex = 13;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txbPiorVolta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txbMelhorVolta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbMedia);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxbTomadaTempo);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox TxbTomadaTempo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbMedia;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txbMelhorVolta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txbPiorVolta;
+        private System.Windows.Forms.Button btnCalcular;
     }
 }
 
--- robertoFormula1/Form1.cs
+++ robertoFormula1/Form1.cs
@@ -12,9 +12,63 @@ namespace robertoFormula1
 {
     public partial class Form1 : Form
     {
+
+        double[] Tempos = new double[5];
+        int i = 0;
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            Tempos[i] = double.Parse(TxbTomadaTempo.Text);
+            i++;
+            TxbTomadaTempo.Text = null;
+            if(i == 5)
+            {
+                MessageBox.Show("Tomada de Tempo cadastradas!");
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            TxbTomadaTempo.Text = null;
+            txbMedia.Text = null;
+            txbMelhorVolta.Text = null;
+            txbPiorVolta.Text = null;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double somaTempos, media, melhorTempo, piorTempo;
+            somaTempos = 0;
+            melhorTempo= 0;
+            piorTempo= 9999999;
+            for(int j = 0; j < Tempos.Length; j++)
+            {
+                somaTempos += Tempos[j];
+                if(melhorTempo < Tempos[j])
+                {
+                    melhorTempo= Tempos[j];
+                }
+                if (piorTempo > Tempos[j])
+                {
+                    piorTempo = Tempos[j];
+                }
+
+            }
+            media = somaTempos/ Tempos.Length;
+            txbMedia.Text = media.ToString();
+            txbMelhorVolta.Text = melhorTempo.ToString();
+            txbPiorVolta.Text = piorTempo.ToString();
+
         }
     }
 }
